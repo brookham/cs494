db65177a2fd2c1993e3b760bbf5173e26784bc53
brookham <bhamilton3264@gmail.com>
2025-09-30 14:07:49 -0700

    Exploration: HTML & CSS

--- exploration1/src/app/page.tsx
+++ exploration1/src/app/page.tsx
@@ -1,10 +1,34 @@
 
-// import styles from "./page.module.css";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>My Exploration!</h1>
+    <div className={styles.myDiv}>
+      <h1 className={styles.myHeader}>My Exploration!</h1>
+      <div>
+        <h3>My Favorite Video Games:</h3>
+        <ul>
+          <li>God of War</li>
+          <li>The Last of Us</li>
+          <li>Project Zomboid</li>
+        </ul>
+      </div>
+      <div>
+        <h3>My Schedule</h3>
+        <table>
+          <thead>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>class</td>
+              <td>class</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
