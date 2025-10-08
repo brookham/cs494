0b9e94206a44126d3138fbad168a63a2f7750c8f
brookham <bhamilton3264@gmail.com>
2025-10-07 19:06:40 -0700

    exploration 2

--- assignment2-1/src/app/page.tsx
+++ assignment2-1/src/app/page.tsx
@@ -4,6 +4,7 @@ import { Carousel } from "react-responsive-carousel";
 
 const url = "https://boringapi.com/api/v1/photos"
 
+
 export default async function Home() {
   const r = await fetch(url)
   const data = await r.json()
@@ -17,24 +18,6 @@ export default async function Home() {
   }
 
   console.log(photoUrls)
-
-  const Carousel = () => {
-    return(
-      <div>
-      <Carousel>
-        <div>
-        {
-          photoUrls.map((x,i)=>(
-            <Image alt={x} key={i} src={x} width={800} height={800} style={{objectFit: "cover"}}/>
-          )
-        )
-        }
-        </div>
-
-      </Carousel>
-      </div>
-    )
-  }
  
   return (
     <main>
