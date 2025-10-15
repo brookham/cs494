a32d84b2172f2136018f8dbfd8ba475db80e2c42
brookham <bhamilton3264@gmail.com>
2025-10-14 22:24:57 -0700

    Exploration 2: Filtering Arrays

--- exploration2/src/components/mtgTable.tsx
+++ exploration2/src/components/mtgTable.tsx
@@ -10,13 +10,48 @@ import { MtgCard } from "@/types/mtgCard"
 
 import Image from "next/image"
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export default function MtgTable(props: { cards: MtgCard[] }) {
 
     const [sortedCards, setSortedCards] = useState<MtgCard[]>(props.cards)
 
+    const [filteredCards, setFilteredCards] = useState<MtgCard[]>(props.cards)
+
     const [ascending, setAscending] = useState<boolean>(true)
+
+    const [colorFilters, setColorFilters] = useState<string[]>([])
+
+    useEffect(()=>{
+        let tmpArray = sortedCards
+
+        for (let i = 0; i < colorFilters.length; i++){
+            tmpArray = tmpArray.filter((card)=>card.manaCost.includes(colorFilters[i]))
+        }
+
+        setFilteredCards([...tmpArray])
+
+    }, [colorFilters, sortedCards])
+
+    function addColorFilter(filter: string){
+        if (!colorFilters.includes(filter)){
+            setColorFilters([...colorFilters, filter])
+        }
+        
+    }
+
+    function deleteColorFilter(filter: string){
+        setColorFilters([...colorFilters.filter(x => x != filter)])
+    }
+    
+    function getElementFromCost(cost: string, i: number, add: boolean){
+        if (["{W}", "{B}", "{U}", "{R}", "{G}"].includes(cost)){
+            return <Image key={i} style={{verticalAlign: "middle", cursor: "pointer"}} onClick={()=>{add ? addColorFilter(cost) : deleteColorFilter(cost)}} alt={cost} width={20} height={20}src={`/images/${cost.slice(1, -1)}.svg`}/>
+        }
+        else {
+            return <span key={i}>{cost}</span>
+        }
+    }
 
     function sort(col: "cmc" | "name" | "oracleText" | "flavorText"){
         if (col == "cmc"){
@@ -38,7 +73,7 @@ export default function MtgTable(props: { cards: MtgCard[] }) {
                     <StyledTableRow>
                         <StyledTableCell>Name<UnfoldMoreIcon sx={{verticalAlign: "middle", cursor: "pointer"}} onClick={()=>{sort("name")}}/></StyledTableCell>
                         <StyledTableCell>Image</StyledTableCell>
-                        <StyledTableCell>ManaCost</StyledTableCell>
+                        <StyledTableCell>ManaCost {colorFilters.map((filter, i) => getElementFromCost(filter, i, false))}</StyledTableCell>
                         <StyledTableCell width={60}>CMC<UnfoldMoreIcon sx={{verticalAlign: "middle", cursor: "pointer"}} onClick={()=>{sort("cmc")}} /></StyledTableCell>
                         <StyledTableCell>Oracle Text<UnfoldMoreIcon sx={{verticalAlign: "middle", cursor: "pointer"}} onClick={()=>{sort("oracleText") }}/></StyledTableCell>
                         <StyledTableCell>Flavor Text<UnfoldMoreIcon sx={{verticalAlign: "middle", cursor: "pointer"}} onClick={()=>{sort("flavorText") }}/></StyledTableCell>
@@ -46,12 +81,12 @@ export default function MtgTable(props: { cards: MtgCard[] }) {
                 </TableHead>
                 <TableBody>
                     {
-                        sortedCards.map((card, i) => (
+                        filteredCards.map((card, i) => (
                             <StyledTableRow key={i}>
                                 <StyledTableCell>{card.name}</StyledTableCell>
                                 <StyledTableCell><Image alt={card.name} width={50} height={70} src={card.image}/></StyledTableCell>
                                 <StyledTableCell>{card.manaCost.map((cost, i) =>
-                                getElementFromCost(cost, i)
+                                getElementFromCost(cost, i, true)
                                 )}</StyledTableCell>
                                 <StyledTableCell>{card.cmc}</StyledTableCell>
                                 <StyledTableCell>{card.oracleText}</StyledTableCell>
@@ -66,9 +101,3 @@ export default function MtgTable(props: { cards: MtgCard[] }) {
     )
 }
 
-function getElementFromCost(cost: string, i: number){
-    if (["{W}", "{B}", "{U}", "{R}", "{G}"].includes(cost)){
-        return <Image key={i} style={{verticalAlign: "middle"}} alt={cost} width={20} height={20}src={`/images/${cost.slice(1, -1)}.svg`}/>
-    }
-    else return <span key={i}>{cost}</span>
-}
