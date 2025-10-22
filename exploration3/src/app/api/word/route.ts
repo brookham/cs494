14fbd623577f36d8a0784f2cf777653c53bcda1c
brookham <bhamilton3264@gmail.com>
2025-10-21 21:04:02 -0700

    Working with an External API

--- exploration3/src/app/api/word/route.ts
+++ exploration3/src/app/api/word/route.ts
@@ -1,9 +1,18 @@
 import { type NextRequest } from "next/server"
 
-export function GET(request: NextRequest){
+export async function GET(request: NextRequest){
     const searchParams = request.nextUrl.searchParams
+    const query = searchParams.get('word') ?? ""
+    const definitions = await getDefinitions(query)
+    return Response.json({definitions: definitions})
+}
 
-    const query = searchParams.get('word')
 
-    return Response.json({message: `Your Word is ${query}`})
+
+async function getDefinitions(word: string){
+    const url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${process.env.API_KEY_DICTIONARY}`
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data.map((datum: any)=>datum.shortdef)
 }
