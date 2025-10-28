cc086d810fd540309ca90e1499d54f9c4184b205
brookham <bhamilton3264@gmail.com>
2025-10-28 14:35:01 -0700

    Caching

--- exploration3/src/app/api/word/route.ts
+++ exploration3/src/app/api/word/route.ts
@@ -2,6 +2,8 @@ import { type NextRequest } from "next/server"
 
 import { Word, wordFromJson } from "@/types/word"
 
+import { redis } from "@/lib/redis"
+
 export async function GET(request: NextRequest){
     const searchParams = request.nextUrl.searchParams
     const query = searchParams.get('word') ?? ""
@@ -13,8 +15,19 @@ export async function GET(request: NextRequest){
 
 async function getDefinitions(word: string){
     const url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${process.env.API_KEY_DICTIONARY}`
+    const cached = await redis.get(word)
+
+    if (cached){
+        console.log("found")
+        return cached
+    }
+
+    console.log("not found")
+
     const response = await fetch(url)
     const data = await response.json()
 
-    return wordFromJson(word, data)
+    const definitions =  wordFromJson(word, data)
+    await redis.set(word, definitions)
+    return definitions
 }
